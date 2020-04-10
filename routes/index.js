11618c1220b2974f4db9412a8cce63c4ed1c397d
Carson Keller <carsonjkeller@gmail.com>
2020-04-10 07:28:58 -0600

    Created GET router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+const { 
+  getArticles,
+  addArticle,
+  updateArticle,
+  deleteArticle
+} = require('../data/works');
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Works' });
+});
+
+router.get('/works', async function(req, res, next) {
+  try {
+    const data = await getArticles();
+    res.render('works', { title: "Works", data: data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Internal Server Issue, check log');
+  };
 });
 
 module.exports = router;
